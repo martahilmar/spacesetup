cc3e0ade31b9a722423fe9beef377faafbcdc5c7
Dennis Hilmar <dennis.hilmar@gmail.com>
2017-12-21 23:41:03 +0100

    More structure!

---
+++ src/posts/posts.js
@@ -0,0 +1,18 @@
+export class Posts {
+  constructor() {
+    this.posts = [
+      {
+        header: 'This is the first post!',
+        scene: 'Insert Photo',
+        content: '...and it started with...'
+      },
+      {
+        header: 'This is the second post!',
+        scene: 'Insert Photo',
+        content: 'This is a post with a LOOOOOOOT of content... ' +
+                 'Maybe even a bit too much content to see all of it in a single tiny little box like this one.' +
+                 'and it started with...'
+      }
+    ];
+  }
+}
